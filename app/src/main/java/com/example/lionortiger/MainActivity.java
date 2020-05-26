17315ae2a136517051415723d31d9fde324803b1
SULTAN <ramhziworld@gmail.com>
2020-05-26 18:23:28 +0100

    Added array to check winner condition

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -12,11 +12,16 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     enum Player {ONE, TWO}
 
+    enum PlayerSelection {LION, TIGER}
+
     Player currentPlayer = Player.ONE;
 
     Player[] playerChoice = new Player[9];
 
-//    int [] [] arrayOfArrays = {{1,2,4},{4,5,6},{7,8,9}};
+    int[][] winnerRowColumn = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+    PlayerSelection[][] playerSelections = new PlayerSelection[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
         tappedImageView.setTranslationX(-2000);
         int tImageTag = Integer.parseInt(tappedImageView.getTag().toString());
         playerChoice[tImageTag] = currentPlayer;
+//        for (int m = 0; m < playerChoice.length; m++)
+//            for (int i = 0; i < 2; i++)
+//                for (int j = 0; j < 2; j++)
+//                    playerSelections[i][j] = playerChoice[tImageTag]);
+
+
         if (currentPlayer == Player.ONE) {
 
             tappedImageView.setImageResource(R.drawable.lion);
